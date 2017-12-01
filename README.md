29cc3a69a855d66a3fe37bea238e391876cac5c1
Steven Saus <steven@stevesaus.com>
2017-11-30 19:57:08 -0500

    updated linux script to work with addin for Firefox Quantum

--- README.md
+++ README.md
@@ -17,6 +17,8 @@ And it turns out the solution to both was the same script file.
 
 Back in 2012, a user named [fehhh](http://ugetdm.com/forum/memberlist.php?mode=viewprofile&u=62) [wrote a script](http://ugetdm.com/forum/viewtopic.php?f=11&t=6) that you could call first. It was later edited by [Michael Tunnell](http://ugetdm.com/forum/memberlist.php?mode=viewprofile&u=2). It relies on [wget](https://www.gnu.org/software/wget/) to determine what kind of file you're downloading. I originally added a few tweaks, then rewrote it and also created a Windows version.
 
+Now uGet has some category matching (yay!) but Firefox Quantum has broken everything (boo!), so I resurrected this to work with an extension that seems to make things work pretty well.
+
 #Linux Instructions
 
 1. Set up uGet with the following categories **in this order** (though the names don't matter)
@@ -25,21 +27,32 @@ Back in 2012, a user named [fehhh](http://ugetdm.com/forum/memberlist.php?mode=v
 	* Docs
 	* Movies
 	* Music
-	* Packages
-	* Torrents
-	* ISOs
 	* Images
+	* Torrents
+
+The "Home" category should point to your "main" download directory.
 
 2. Download the [uget_preprocessor.sh](https://raw.githubusercontent.com/uriel1998/uget_autosorter/master/uget_preprocessor.sh) script.  
 3. Give the script executable permissions: ```chmod a+x uget_preprocessor.sh```  
 4. Make sure you don't actually have KGet installed at this point.   
-5. ```sudo cp uget_preprocessor.sh /usr/bin/kget```  
-6. Set FlashGot (or whatever other application) to use *KGet* as the downloader.  
-7. If there's an option for commandline options, use ```[URL] --filename=[FNAME]```
+5. ```sudo ln -s uget_preprocessor.sh /usr/bin/kget```  
+6. Setup [Download with Wget](https://addons.mozilla.org/en-US/firefox/addon/download-with-gnu-wget/) - *including the helper application* - to use *KGet* (or the script itself) as the downloader.  
+7. If there's an option for commandline options, the following are available:
+
+* -u URL: The url to download
+* -f FILENAME: The suggested filename
+* -r REFERRER: Referrer link
+* -c COOKIEFILE: Cookies
+
+If no options are specified, the first parameter is assumed to be the URL to download.
+
 8. Profit!  
 
 
-#Windows Instructions
+# Windows Instructions  
+
+## EDIT:  Errrrr, sorry, I'm focused on trying to make this work for me on Linux.  I've left the Windows code here, but I'm not hugely motivated to fight with it again.  
+
 
 Wait! You use Windows? Guess what! You're in luck too! uGet is a portable package for Windows, so you can use it as well. The only difference is that there's two basic tools that you'll need to snag, both free and open source. (Please note that if you deviate from these instructions, you'll have to change the script; some file paths are hardcoded in the beginning.)
 
@@ -50,10 +63,8 @@ Wait! You use Windows? Guess what! You're in luck too! uGet is a portable packag
 	* Docs
 	* Movies
 	* Music
-	* Packages
-	* Torrents
-	* ISOs
 	* Images
+	* Torrents
 3. Get and install the **setup** packages of [Win32 WGet](http://gnuwin32.sourceforge.net/packages/wget.htm) and [Win32 Grep](http://gnuwin32.sourceforge.net/packages/grep.htm).
 4. Download the [uget_preprocessor.cmd](https://raw.githubusercontent.com/uriel1998/uget_autosorter/master/uget_preprocessor.cmd) script.  
 5. Copy ```uget_preprocessor.cmd``` to ```C:\uGet```
--- uget_preprocessor.sh
+++ uget_preprocessor.sh
@@ -1,75 +1,159 @@
 #!/bin/bash
-# uGet_category_selection  script originally by fehhh & Michael Tunnell
+# uGet_category_selection   script originally by fehhh & Michael Tunnell
 # Rewritten with case statements and expanded MIME parsing by Steven Saus
-#
+
+# Declarations
+URL=""
+FILENAME=""
+REFERER=""
+COOKIES=""
+CATEGORY=""
+MAINDIR=""
+SUGGESTEDFN=""
+
+function call_uget {
+
+    uget-gtk "$REFERER" "$COOKIES" "$FILENAME" "$URL" "$CATEGORY"
+}
 
 # Used when application/octet-stream or just major mimetype of application that
 # doesn't match one of the things we've set up.
 # Much easier to parse in bash than Windows, let me tell you.
 function match_ext() {
-  if echo "$extension" | egrep -i -e '\.(torrent|magnet)'; then
-    uget-gtk $2 $1 --category-index=6
-  elif echo "$extension" | egrep -i -e '\.(iso|img|cue)'; then
-    uget-gtk $2 $1 --category-index=7
-  elif echo "$extension" | egrep -i -e '\.(7z|zip|tar|gz|rar|arj|bz2|tgz)'; then
-    uget-gtk $2 $1 --category-index=1
-  elif echo "$extension" | egrep -i -e '\.(doc|docx|xls|xlsx|odt|odf|css|html|djvu|rtf|csv|tsv|pdf|epub|azw|kf8)'; then
-    uget-gtk $2 $1 --category-index=2
-  elif echo "$extension" | egrep -i -e '\.(avi|mov|m4v|mpg|mpeg|flv|mkv|wmv|mp4)'; then
-    uget-gtk $2 $1 --category-index=3
-  elif echo "$extension" | egrep -i -e '\.(mp3|midi|mod|s3m|wav|ogg|m3u|pls|flac|ape|m4a)'; then
-    uget-gtk $2 $1 --category-index=4
-  elif echo "$extension" | egrep -i -e '\.(apk|pup|pet|ebuild|appx|appxbundle|deb|rpm|yum|msi)'; then
-    uget-gtk $2 $1 --category-index=5
-  elif echo "$extension" | egrep -i -e '\.(gif|jpg|jpeg|svg|png|tiff|tif|ico|pbm|pcx)'; then
-    uget-gtk $2 $1 --category-index=8
-  else
-    uget-gtk $2 $1 --category-index=0
-  fi
+
+    if echo "$extension" | egrep -i -e '\.(torrent|magnet)'; then
+        CATEGORY="--category-index=6"
+    elif echo "$extension" | egrep -i -e '\.(iso|img|cue)'; then
+        CATEGORY="--category-index=1"
+    elif echo "$extension" | egrep -i -e '\.(7z|zip|tar|gz|rar|arj|bz2|tgz|xz)'; then
+        CATEGORY="--category-index=1"
+    elif echo "$extension" | egrep -i -e '\.(doc|docx|xls|xlsx|odt|odf|css|html|djvu|rtf|csv|tsv|pdf|epub|azw|kf8|css|csv|djvu|vcf|md)'; then
+        CATEGORY="--category-index=2"
+    elif echo "$extension" | egrep -i -e '\.(avi|mov|m4v|mpg|mpeg|flv|mkv|wmv|mp4)'; then
+        CATEGORY="--category-index=3"
+    elif echo "$extension" | egrep -i -e '\.(mp3|midi|mod|s3m|wav|ogg|m3u|pls|flac|ape|m4a)'; then
+        CATEGORY="--category-index=4"
+    elif echo "$extension" | egrep -i -e '\.(apk|pup|pet|ebuild|appx|appxbundle|deb|rpm|yum|msi|bin|exe)'; then
+        CATEGORY="--category-index=1"
+    elif echo "$extension" | egrep -i -e '\.(xcf|gif|jpg|jpeg|svg|png|tiff|tif|ico|pbm|pcx)'; then
+        CATEGORY="--category-index=5"
+    else
+        CATEGORY="--category-index=0"
+    fi
+}
+
+
+function show_help {
+
+echo -e "\n -u [URL] -f [FILENAME] -r [REFERRER] -c [COOKIEFILE] \n\n" \
+    "See the README about setting up categories in uGet \n"\
+    "Designed to be used with the Download with WGet extension \n"\   
+    "Category sorting complements that of what is built into uGet now. \n"\ 
+    "Works with Firefox Quantum \n"\
+    "-h shows this help message\n"
 }
 
 
 # If you're reading along, we start here.
 # Wget getting info about file types
-DINFO=$(wget --spider "$1" 2>&1)
-mimetype=$(echo "$DINFO"|egrep -e '^Length:'|awk -F "[" '{print $2}'|awk -F "]" '{print $1}')
-mainmimetype=$(echo "$mimetype"|awk -F "/" '{print $1}')
-fileurl=$(echo "$DINFO"|egrep -e '^--'|awk -F " " '{print $3}')
-file=$(basename $fileurl)
-extension=${file##*.}
-# need to put in check for if there's a passed suggested filename
-# and see if those extensions match as an error check
-# just in case it's a weird stream or something
 
-case "$mainmimetype" in
-  "text")
-    uget-gtk $2 $1 --category-index=2
-  ;;
-  "video")
-    uget-gtk $2 $1 --category-index=3
-  ;;
-  "audio")
-    uget-gtk $2 $1 --category-index=4
-  ;;
-  "images")
-    uget-gtk $2 $1 --category-index=8
-  ;;
-  *)
-    #Now for the if-elif stuff
-    if echo "$mimetype" | egrep -e 'application\/x.bittorrent'; then
-      uget-gtk $2 $1 --category-index=6
-    elif echo "$mimetype" | egrep -e 'application\/x-cd-image'; then
-      uget-gtk $2 $1 --category-index=7
-    elif echo "$mimetype" | egrep -e 'application\/(x.bzip2|x.gzip|x.tar|x.7z|rar|zip|x.bzip|x.gtar|x.compressed|lha|x.lha|x.compress|a.gzip|x.gunzip)'; then
-      uget-gtk $2 $1 --category-index=1
-    elif echo "$mimetype" | egrep -e 'application\/(vnd.ms.powerpoint|vnd.ms.excel|x.excel|x.ms.excel|vnd.oasis.opendocument.text|vnd.opendocument.spreadsheet|vnd.oasis.opendocument.presentation|vnd.oasis.opendocument.graphics|pdf|msword|x.rtf|mswrite|x-wri|rtf|vnd.ms.project|x.mspowerpoint|powerpoint|vnd.djvu|xml|epub+zip|vnd.amazon.ebook|x.mobipocket.ebook)'; then
-      uget-gtk $2 $1 --category-index=2
-    elif echo "$mimetype" | egrep -e 'application\/(x.debian.package|x.redhat.package.manager|x.rpm|x.msi)'; then
-      uget-gtk $2 $1 --category-index=5
-    elif echo "$mimetype" | egrep -e 'application\/octet.stream'; then
-      match_ext
+function main_match {
+
+    DINFO=$(wget --spider "$URL" 2>&1)
+    mimetype=$(echo "$DINFO"|egrep -e '^Length:'|awk -F "[" '{print $2}'|awk -F "]" '{print $1}')
+    mainmimetype=$(echo "$mimetype"|awk -F "/" '{print $1}')
+    fileurl=$(echo "$DINFO"|egrep -e '^--'|awk -F " " '{print $3}')
+    file=$(basename $fileurl)
+    urlextension=${file##*.}
+    
+    # Logic: If there's a suggested filename, and they don't match, see if the URL based
+    # extension is matched by one of our known extensions. If not, then use the suggested
+    # filename.  Otherwise, use the URL based one.
+    
+    if [ -n "$SUGGESTEDFN" ];then
+        extension2=${SUGGESTEDFN##*.}
+        if [ "$urlextension" != "$extension2" ];then
+            if echo "$urlextension" | egrep -i -e '\.(torrent|magnet|iso|img|cue|7z|zip|tar|gz|rar|arj|bz2|tgz|xz|doc|docx|xls|xlsx|odt|odf|css|html|djvu|rtf|csv|tsv|pdf|epub|azw|kf8|css|csv|djvu|vcf|md|avi|mov|m4v|mpg|mpeg|flv|mkv|wmv|mp4|mp3|midi|mod|s3m|wav|ogg|m3u|pls|flac|ape|m4a|apk|pup|pet|ebuild|appx|appxbundle|deb|rpm|yum|msi|bin|exe|xcf|gif|jpg|jpeg|svg|png|tiff|tif|ico|pbm|pcx)'; then
+                extension=$(echo "$urlextension")
+            else
+                extension=$(echo "$extension2")
+            fi
+        fi
     else
-      match_ext
+        extension=$(echo "$urlextension")
     fi
-  ;;
-esac
+
+    # Matching main mime type first, failing that, the minor mimetype, 
+    # then failing that, the extension matching.
+
+    case "$mainmimetype" in
+        "text")
+            CATEGORY="--category-index=2"
+        ;;
+        "video")
+            CATEGORY="--category-index=3"
+        ;;
+        "audio")
+            CATEGORY="--category-index=4"
+        ;;
+        "images")
+            CATEGORY="--category-index=5"
+        ;;
+        *)
+            #Now for the if-elif stuff
+            if echo "$mimetype" | egrep -e 'application\/x.bittorrent'; then
+                CATEGORY="--category-index=6"
+            elif echo "$mimetype" | egrep -e 'application\/x-cd-image'; then
+                CATEGORY="--category-index=1"
+            elif echo "$mimetype" | egrep -e 'application\/(x.bzip2|x.gzip|x.tar|x.7z|rar|zip|x.bzip|x.gtar|x.compressed|lha|x.lha|x.compress|a.gzip|x.gunzip)'; then
+                CATEGORY="--category-index=1"
+            elif echo "$mimetype" | egrep -e 'application\/(vnd.ms.powerpoint|vnd.ms.excel|x.excel|x.ms.excel|vnd.oasis.opendocument.text|vnd.opendocument.spreadsheet|vnd.oasis.opendocument.presentation|vnd.oasis.opendocument.graphics|pdf|msword|x.rtf|mswrite|x-wri|rtf|vnd.ms.project|x.mspowerpoint|powerpoint|vnd.djvu|xml|epub+zip|vnd.amazon.ebook|x.mobipocket.ebook)'; then
+                CATEGORY="--category-index=2"
+            elif echo "$mimetype" | egrep -e 'application\/(x.debian.package|x.redhat.package.manager|x.rpm|x.msi)'; then
+                CATEGORY="--category-index=1"
+            elif echo "$mimetype" | egrep -e 'application\/octet.stream'; then
+                match_ext
+            else
+                match_ext
+            fi
+        ;;
+    esac
+}
+
+
+# The base uGet category is considered the "Home" category and used to determine the default d/l location for stripping off that path.
+HOMEDIR=$(grep $HOME/.config/uGet/category/0000.json -e "folder" | awk -F '"' '{print $4"/"}' | sed -e 's/\\//g')
+
+
+while getopts "u:f:r:c:h" opt; do
+    case $opt in
+        u)  URL=$(echo "$OPTARG")
+            ;;
+        f)  tempfilename=$(echo "$OPTARG")
+            SUGGESTEDFN=${tempfilename#${HOMEDIR}}
+            FILENAME=$(echo "--filename=$SUGGESTEDFN")
+            ;;
+        r)  REFERER=$(echo "--http-referer=$OPTARG")
+            ;;
+        c)  COOKIES=$(echo "--http-cookie-file=$OPTARG")
+            ;;
+        h)  show_help
+            exit
+            ;;        
+    esac
+done
+
+#for backwards compatibility
+
+if [ -z "$URL" ];then
+    URL="$1"
+fi
+
+main_match
+
+if [ -z "$URL" ];then
+    show_help
+    exit
+else
+    call_uget
+fi
